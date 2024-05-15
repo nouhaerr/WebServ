3e94aaa67b40d48a7ce42991a79d8519c76309e8
Kerberos <user@MacBook-Air-van-User.local>
2024-05-15 18:22:56 +0100

    mergin/feat1

---
+++ 2024/src/parsing/ConfigServer.hpp
@@ -0,0 +1,71 @@
+#ifndef CONFIGSERVER_HPP
+# define CONFIGSERVER_HPP
+
+#include "../Macros.hpp"
+#include <stdexcept>
+#include <cstdlib>
+
+#include "Config.hpp"
+#include "ConfigLocation.hpp"
+
+class Config;
+class ConfigLocation;
+class	ConfigServer {
+	public:
+		ConfigServer();
+		~ConfigServer();
+		ConfigServer(ConfigServer const &src);
+		ConfigServer&	operator=(const ConfigServer &src);
+
+		ConfigLocation	parseLocation(std::vector<t_tokens> &tok, std::vector<t_tokens>::iterator& it);
+		
+		//Getters
+		const std::string			&getHost() const;
+		const size_t				&getPort() const;
+		const std::string			&getServerName() const;
+		const size_t				&getMaxBodySize() const;
+		std::vector<ConfigLocation>	&getLocation();
+		bool						&getAutoIndex();
+		std::string					&getRoot();
+		std::map<int, std::string>	&getErrorPage();
+
+		// Setters
+		void	setListen(std::string& listen);
+		void	setServerName(std::string& serverName);
+		void	setBodySize(std::string& bodySize);
+		void	setLocation(std::vector<t_tokens> &tok, std::vector<t_tokens>::iterator& it);
+		void	setAutoIndex(std::string& autoindex);
+		void	setRoot(std::string& root);
+		void	setErrorPage(std::string& errorPage);
+
+		class	ConfigServerException: public std::exception{
+			std::string message;
+			public:
+				ConfigServerException(std::string msg) : message(msg){};
+			const char *what() const throw()
+			{
+				return (this->message.c_str());
+			}
+			~ConfigServerException() throw(){};
+		};
+
+    private:
+        std::string					_host;
+        size_t						_port;
+        std::string					_serverName;
+        size_t						_maxBodySize;
+		std::vector<ConfigLocation> _location;
+		bool						_autoindex;
+		std::string					_root;
+		std::vector<std::string>	_index;
+		std::map<int, std::string>	_errorPage;
+};
+
+bool						isValidIPAddress(const std::string& ip);
+size_t						parseMaxBodySize(char &unit, std::string& bodySize, size_t sizeEnd);
+std::vector<std::string>	splitVal(std::string& str);
+size_t						isNum(std::string str);
+bool						isUrl(const std::string& str);
+std::vector<std::string>	splitArgs(std::string val);
+
+#endif
